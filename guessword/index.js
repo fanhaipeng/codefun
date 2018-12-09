868a1b5067db786a7d160dda23d2ec11c6c4e40d
Haipeng Fan <fanhaipeng@gmail.com>
2018-12-08 22:50:14 -0800

    Add basic guessing logic

--- guessword/index.js
+++ guessword/index.js
@@ -1,7 +1,24 @@
 var vm = new Vue({
     el: '#guessword',
     data:{
-        word: 'remarkable',
+        word: [{ value: 'b', guessed: false},
+               { value: 'o', guessed: false},
+               { value: 'o', guessed: false},
+               { value: 'k', guessed: false},
+               { value: 'c', guessed: false},
+               { value: 'a', guessed: false},
+               { value: 's', guessed: false},
+               { value: 'e', guessed: false}
+            ],
         letters: 'abcdefghijklmnopqrstuvwxyz'.split('')
+    },
+    methods:{
+        letterClicked: function(letter) {
+            for (let l of this.word){
+                if (letter === l.value){
+                    l.guessed = true;
+                }
+            }
+        }
     }
 });
