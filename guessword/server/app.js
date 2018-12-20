64dd2f844de1b6ed1967166e31235744525c9e50
Haipeng Fan <hafan@microsoft.com>
2018-12-19 16:49:22 -0800

    Add css file for mobile device

--- guessword/server/app.js
+++ guessword/server/app.js
@@ -1,9 +1,11 @@
 var path = require('path');
 var express = require('express');
+var device = require('express-device');
 var app = express();
 var wordslist = require('./wordslist.js');
 
 var rootPath = __dirname + path.sep + '..' + path.sep;
+app.use(device.capture());
 app.use(express.static(rootPath));
 
 app.get('/', function(req, res){
@@ -18,10 +20,15 @@ app.get('/next', function(req, res){
     res.end();
 });
 
-app.get('index.js', function(req, res){
-    res.sendFile('index.js', {root : rootPath}, function(err){
+app.get('/main.css', function(req, res){
+    let fileName = 'main-mobile.css';
+    if (req.device.type === 'desktop'){
+        fileName = 'main-desktop.css';
+    }
+
+    res.sendFile(fileName, {root: rootPath}, function(err){
         console.error(err);
-    });
-})
+    })
+});
 
 app.listen(3000);
