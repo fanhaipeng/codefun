b617db5d660faf2141dc63c0c5f829e7dc86987f
Haipeng Fan <fanhaipeng@gmail.com>
2019-01-03 22:20:22 -0800

    [Spelling] Add word-set-selector component

--- spelling/app/src/App.vue
+++ spelling/app/src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <WordSetSelector ref="wordSetSelector"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WordSetSelector from './components/WordSetSelector.vue'
 
 export default {
   name: 'app',
+  mounted: function(){
+    this.$refs.wordSetSelector.init();
+  },
+  methods: {
+  },
   components: {
-    HelloWorld
+    WordSetSelector
   }
 }
 </script>
---
+++ spelling/app/src/components/WordSetSelector.vue
@@ -0,0 +1,26 @@
+<template>
+<div v-on:load-wordset="init">
+    <div>Select a Word List</div>
+    <ul>
+    </ul>
+</div>    
+</template>
+
+<script>
+export default {
+    name: "WordSetSelector",
+    props: {
+    },
+    methods:{
+        init: function(){
+            console.log('word list initialized');
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
+
+
