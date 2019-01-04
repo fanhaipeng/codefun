4bb7f7e0347d614dae19829bed4c56ad07284af2
Haipeng Fan <fanhaipeng@gmail.com>
2019-01-04 12:05:23 -0800

    [Spelling] allow speech service token to be used multiple times

--- spelling/server/tts.js
+++ spelling/server/tts.js
@@ -1,82 +1,104 @@
-const https = require('https');
-const fs = require('fs');
-const http = require('http');
+const https = require("https");
+const fs = require("fs");
+const http = require("http");
 
+const subscriptionKey = "4ec4d0210b214c669885bb9ac842ede9";
+const tokenUri =
+  "https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken";
+
+var sessionToken;
+var sesstionTokenTimeStamp = 0;
+const EightMinutesMilliSeconds = 1000 * 60 * 8;
 function getToken() {
-    const subscriptionKey = "4ec4d0210b214c669885bb9ac842ede9";
-    const tokenUri = 'https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken';
-    return new Promise(function (resolve, reject) {
-        let tokenRequestOptions = {
-            method: 'POST',
-            headers: {
-                'Ocp-Apim-Subscription-Key': subscriptionKey
-            }
-        };
+  return new Promise(function(resolve, reject) {
+    let currentTime = Date.now();
+    if (
+      sessionToken &&
+      currentTime - sesstionTokenTimeStamp < EightMinutesMilliSeconds
+    ) {
+      return resolve(sessionToken);
+    }
 
-        let sessionToken = '';
-        let request = https.request(tokenUri, tokenRequestOptions, (res) => {
-            if (res.statusCode !== 200) {
-                reject(`Fail to get access token, REQUEST STATUS: ${res.statusCode}`);
-            }
+    let tokenRequestOptions = {
+      method: "POST",
+      headers: {
+        "Ocp-Apim-Subscription-Key": subscriptionKey
+      }
+    };
 
-            res.setEncoding('utf8');
-            res.on('data', (chunk) => {
-                sessionToken += chunk;
-            })
+    let request = https.request(tokenUri, tokenRequestOptions, res => {
+      if (res.statusCode !== 200) {
+        reject(`Fail to get access token, REQUEST STATUS: ${res.statusCode}`);
+      }
 
-            res.on('end', () => {
-                resolve(sessionToken);
-            })
-        });
+      res.setEncoding("utf8");
+      res.on("data", chunk => {
+        sessionToken += chunk;
+      });
 
-        request.on('error', (e) => {
-            reject(e);
-        })
-
-        request.write('');
-        request.end();
+      res.on("end", () => {
+        resolve(sessionToken);
+      });
     });
+
+    request.on("error", e => {
+      reject(e);
+    });
+
+    request.write("");
+    request.end();
+  });
 }
 
-function t2s(accessToken, text){
-    return new Promise(function (resolve, reject) {
-        var msgBody = `<speak version='1.0' xml:lang='en-US'><voice xml:lang='en-US' xml:gender='Female' name='Microsoft Server Speech Text to Speech Voice (en-US, JessaRUS)'>${text}</voice></speak>`;
-        let requestOptions = {
-            method: 'POST',
-            headers : {
-                'X-Microsoft-OutputFormat' : 'audio-16khz-128kbitrate-mono-mp3',
-                'Content-Type'  : 'application/ssml+xml',
-                'Authorization'  : `Bearer ${accessToken}`,
-                'User-Agent'    : 'CodeFunSpeech'
-            }
-        };
+function t2s(accessToken, text) {
+  return new Promise(function(resolve, reject) {
+    var msgBody = `<speak version='1.0' xml:lang='en-US'><voice xml:lang='en-US' xml:gender='Female' name='Microsoft Server Speech Text to Speech Voice (en-US, JessaRUS)'>${text}</voice></speak>`;
+    let requestOptions = {
+      method: "POST",
+      headers: {
+        "X-Microsoft-OutputFormat": "audio-16khz-128kbitrate-mono-mp3",
+        "Content-Type": "application/ssml+xml",
+        Authorization: `Bearer ${accessToken}`,
+        "User-Agent": "CodeFunSpeech"
+      }
+    };
 
-        let request = https.request('https://westus.tts.speech.microsoft.com/cognitiveservices/v1', requestOptions, (res) => {
-            if (res.statusCode !== 200){
-                reject(`ERROR: ${res.statusCode}`);
-            } else {
-                resolve(res);
-            }
-        });
+    let request = https.request(
+      "https://westus.tts.speech.microsoft.com/cognitiveservices/v1",
+      requestOptions,
+      res => {
+        if (res.statusCode !== 200) {
+          reject(`ERROR: ${res.statusCode}`);
+        } else {
+          resolve(res);
+        }
+      }
+    );
 
-
-        request.on('error', (err) => {
-            reject(err);
-        })
-
-        request.write(msgBody);
-        request.end();
+    request.on("error", err => {
+      reject(err);
     });
+
+    request.write(msgBody);
+    request.end();
+  });
 }
 
 module.exports = {
-    text2speech: async function (word) {
-        return getToken()
-            .then((val) => { return t2s(val, word); },
-                  (err) => { console.error(`ERROR: ${err}`); return Promise.reject(err); })
-            .catch((err) => {
-                console.error(`ERROR: ${err}`);
-                return Promise.reject(err);
-            });
-    }
-}
+  text2speech: async function(word) {
+    return getToken()
+      .then(
+        val => {
+          return t2s(val, word);
+        },
+        err => {
+          console.error(`ERROR: ${err}`);
+          return Promise.reject(err);
+        }
+      )
+      .catch(err => {
+        console.error(`ERROR: ${err}`);
+        return Promise.reject(err);
+      });
+  }
+};
