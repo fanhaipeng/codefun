8a56db7cf32ea2d0ba0bb0f52e9699362b9cfed7
Haipeng Fan <fanhaipeng@gmail.com>
2019-01-04 22:30:57 -0800

    [Spelling] Add server interface to add words list

--- spelling/server/app.js
+++ spelling/server/app.js
@@ -1,14 +1,32 @@
-const express = require("express");
-const blob = require("./azure-blob");
+const express = require("express")
+const blob = require("./azure-blob")
+const bodyParser = require('body-parser')
 
 var app = express();
+app.use(bodyParser.text());
 
 app.get("/", (req, res) => {
   res.sendFile("index.html", { root: __dirname });
 });
 
 app.get("/wordlists", getWordLists);
-app.listen(process.env.PORT || 8888);
+app.put('/wordlist/:listname', addWordList);
+app.listen(process.env.PORT || 8000);
+
+function addWordList(req, res){
+  let data = {
+    name: req.params.listname,
+    list: req.body.split('\r\n')
+  }
+
+  blob.addWordsList(data)
+  .then(result => {
+    res.status(201).end()
+  })
+  .catch(err => {
+    res.status(500).send('Something went wrong, error: ' + err)
+  })
+}
 
 function getWordLists(req, res) {
   blob
