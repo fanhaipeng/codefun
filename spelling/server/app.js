b5f5d430b79f4ee891afc144095dd0abe29483e5
Haipeng Fan <fanhaipeng@gmail.com>
2019-01-04 17:55:50 -0800

    [Spelling] Add get wordlists interface on server side

--- spelling/server/app.js
+++ spelling/server/app.js
@@ -1,9 +1,22 @@
-const express = require('express');
+const express = require("express");
+const blob = require("./azure-blob");
 
 var app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html', { root : __dirname })
-})
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname });
+});
 
+app.get("/wordlists", getWordLists);
 app.listen(process.env.PORT || 8888);
+
+function getWordLists(req, res) {
+  blob
+    .getWordsLists()
+    .then(lists => {
+      res.send(lists);
+    })
+    .catch(err => {
+      res.sendStatus(500).send("Something went wrong, error: " + err);
+    });
+}
